f58f9d105973e34788aa05ac8edaeb754bed9ad9
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-06 17:51:57 +1200

    add spatial pyramid pooling

--- layers.py
+++ layers.py
@@ -376,3 +376,81 @@ class FilterDetections(keras.layers.Layer):
         })
 
         return config
+
+# NOTE: ADDED
+class SpatialPyramidPooling(keras.layers.Layer):
+    """Spatial pyramid pooling layer for 2D inputs.
+    See Spatial Pyramid Pooling in Deep Convolutional Networks for Visual Recognition,
+    K. He, X. Zhang, S. Ren, J. Sun
+    # Arguments
+        pool_list: list of int
+            List of pooling regions to use. The length of the list is the number of pooling regions,
+            each int in the list is the number of regions in that pool. For example [1,2,4] would be 3
+            regions with 1, 2x2 and 4x4 max pools, so 21 outputs per feature map
+    # Input shape
+        4D tensor with shape:
+        `(samples, channels, rows, cols)` if dim_ordering='th'
+        or 4D tensor with shape:
+        `(samples, rows, cols, channels)` if dim_ordering='tf'.
+    # Output shape
+        2D tensor with shape:
+        `(samples, channels * sum([i * i for i in pool_list])`
+    """
+
+    def __init__(self, pool_list, **kwargs):
+        self.pool_list = pool_list
+        self.num_outputs_per_channel = sum([i * i for i in pool_list])
+        super(SpatialPyramidPooling, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.nb_channels = input_shape[3]
+
+    def compute_output_shape(self, input_shape):
+        return (input_shape[0], self.nb_channels * self.num_outputs_per_channel)
+
+    def get_config(self):
+        config = {'pool_list': self.pool_list}
+        base_config = super(SpatialPyramidPooling, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    def call(self, x, mask=None):
+
+        input_shape = tf.shape(x)
+        num_rows = input_shape[1]
+        num_cols = input_shape[2]
+
+        row_length = [keras.backend.cast(num_rows, 'float32') / i for i in self.pool_list]
+        col_length = [keras.backend.cast(num_cols, 'float32') / i for i in self.pool_list]
+
+        outputs = []
+
+        for pool_num, num_pool_regions in enumerate(self.pool_list):
+            for jy in range(num_pool_regions):
+                for ix in range(num_pool_regions):
+                    x1 = ix * col_length[pool_num]
+                    x2 = ix * col_length[pool_num] + col_length[pool_num]
+                    y1 = jy * row_length[pool_num]
+                    y2 = jy * row_length[pool_num] + row_length[pool_num]
+
+                    x1 = keras.backend.cast(keras.backend.round(x1), 'int32')
+                    x2 = keras.backend.cast(keras.backend.round(x2), 'int32')
+                    y1 = keras.backend.cast(keras.backend.round(y1), 'int32')
+                    y2 = keras.backend.cast(keras.backend.round(y2), 'int32')
+
+                    new_shape = [y2 - y1,
+                                    x2 - x1, input_shape[3]]
+
+                    x_crop = x[:, y1:y2, x1:x2, :]
+                    xm = keras.layers.Reshape(new_shape)(x_crop)
+                    pooled_val = keras.backend.max(xm, axis=(1, 2))
+                    outputs.append(pooled_val)
+
+
+        outputs = keras.backend.concatenate(outputs, axis=1)
+        outputs = keras.layers.Reshape((self.num_outputs_per_channel, self.nb_channels))(outputs)
+        # outputs = K.squeeze(outputs, axis=1) # makes output only rank 4, instead of 5
+        # outputs = concatenate(outputs)
+        # outputs = Reshape((len(self.pool_list),self.num_outputs_per_channel,input_shape[0],input_shape[1]))(outputs)
+        # outputs = Permute(outputs,(3,1,0,2))
+
+        return outputs
