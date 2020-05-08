d4d430e919a173f2e4b78ed7187cf61837b1a062
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-08 15:05:21 +1200

    add script

---
+++ train.sh
@@ -0,0 +1,15 @@
+# script to set variables more easily - note steps * batch_size <= training samples
+python3 train.py \
+    --detect-quadrangle \
+    --snapshot imagenet \
+    --phi 0 \
+    --gpu 0 \
+    --random-transform \
+    --compute-val-loss \
+    --freeze-backbone \
+    --batch-size 32 \
+    --steps 10 \
+    --hinge_loss \
+    --wandb \
+    csv data/train.csv data/classes.csv data/colors.csv data/bodies.csv \
+    --val-annotations data/val.csv
