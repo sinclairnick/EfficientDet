ba10c396fb94da4aa958957ec9f7af34ddddae5b
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-10 14:43:17 +1200

    add more training scripts

--- train-cropped-rect.sh
+++ train-cropped-rect.sh
@@ -1,6 +1,5 @@
 # script to set variables more easily - note steps * batch_size <= training samples
 python3 train.py \
-    --detect-quadrangle \
     --snapshot imagenet \
     --phi 0 \
     --gpu 0 \
@@ -11,9 +10,9 @@ python3 train.py \
     --batch-size 35 \
     --lr 0.001 \
     --steps 10 \
-    --epochs 100 \
+    --epochs 200 \
     --hinge_loss \
     --dropout_rate 0.1 \
     --wandb \
-    csv data/train.csv data/classes.csv data/colors.csv data/bodies.csv \
-    --val-annotations data/val.csv
+    csv data/train-cropped-rect.csv data/classes.csv data/colors.csv data/bodies.csv \
+    --val-annotations data/val-cropped-rect.csv
--- train-rect.sh
+++ train-rect.sh
@@ -1,8 +1,7 @@
 # script to set variables more easily - note steps * batch_size <= training samples
 python3 train.py \
-    --detect-quadrangle \
     --snapshot imagenet \
-    --phi 0 \
+    --phi 4 \
     --gpu 0 \
     --weighted-bifpn \
     --random-transform \
@@ -15,5 +14,5 @@ python3 train.py \
     --hinge_loss \
     --dropout_rate 0.1 \
     --wandb \
-    csv data/train.csv data/classes.csv data/colors.csv data/bodies.csv \
-    --val-annotations data/val.csv
+    csv data/train-rect.csv data/classes.csv data/colors.csv data/bodies.csv \
+    --val-annotations data/val-rect.csv
