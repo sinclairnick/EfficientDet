ff9f14e8a81c6ed22e1e730fb068e9a4f4c395da
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-12 10:30:24 +1200

    move constants to top

--- train.sh
+++ train.sh
@@ -1,18 +1,18 @@
 # script to set variables more easily - note steps * batch_size <= training samples
 python3 train.py \
-    --phi 0 \
     --gpu 0 \
     --freeze-backbone \
-    --snapshot coco \
     --weighted-bifpn \
     --compute-val-loss \
     --random-transform \
+    --wandb \
     --batch-size 32 \
+    --snapshot coco \
+    --phi 0 \
     --lr 0.001 \
     --steps 10 \
     --epochs 200 \
     --hinge_loss \
     --dropout_rate 0.5 \
-    --wandb \
     csv data/train-annotations.csv data/classes.csv data/colors.csv data/bodies.csv \
     --val-annotations data/val-annotations.csv
