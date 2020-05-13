c5df2d412549eb563aaba64ba97ba3dee9ac61e9
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-13 18:30:40 +1200

    fix bounding box issue

--- data/src/make-data.py
+++ data/src/make-data.py
@@ -3,7 +3,8 @@ import numpy as np
 from utils import (
     get_body_names, get_colors_names,
     align_stanford_classes, remove_extraneous,
-    tlhw_to_corners, save_dataset, split, mkdir
+    tlhw_to_corners, save_dataset, split, mkdir,
+    nzvd_pipeline
     )
 from common import input_dir, output_dir, TLHW
 
@@ -13,24 +14,16 @@ if __name__ == '__main__':
     nzvd_train = pd.read_csv(f'{input_dir}/nzvd/train_labels.csv')
     nzvd_test = pd.read_csv(f'{input_dir}/nzvd/test_labels.csv')
 
-    stan = pd.read_csv(f'{input_dir}/stanford-cars/anno_train.csv')
+    stan = pd.read_csv(f'{input_dir}/stanford-cars/anno_train.csv', header=None)
     stan.columns = ['file', *TLHW, 'class']
+
+    nzvd_train, nzvd_test = nzvd_pipeline(nzvd_train), nzvd_pipeline(nzvd_test)
 
     all_data = nzvd_train.append(nzvd_test, ignore_index=True)
     all_bodies = np.unique(get_body_names(all_data))
     all_colors = np.unique(get_colors_names(all_data))
 
     stan = align_stanford_classes(stan, all_bodies)
-    print('Stanford cars subset length: {}'.format(len(stan)))
-    nzvd_train = remove_extraneous(nzvd_train)
-    nzvd_test = remove_extraneous(nzvd_test)
-
-    assert np.all(np.equal(nzvd_train.columns, nzvd_test.columns)), "Column names must match"
-    assert np.all(np.equal(nzvd_train.columns, stan.columns)), "Column names must match"
-
-    nzvd_train = tlhw_to_corners(nzvd_train)
-    nzvd_test = tlhw_to_corners(nzvd_test)
-    stan = tlhw_to_corners(stan)
 
     nzvd_train, nzvd_val = split(nzvd_train)
     assert abs(len(nzvd_val) * 9 - len(nzvd_train)) < 10
@@ -38,13 +31,16 @@ if __name__ == '__main__':
     stan_train, stan_val = split(stan)
     assert abs(len(stan_val) * 9 - len(stan_train)) < 10
 
+    assert np.all(np.equal(nzvd_train.columns, nzvd_test.columns)), "Column names must match"
+    assert np.all(np.equal(nzvd_train.columns, stan.columns)), "Column names must match"
+
     save_dataset(nzvd_train, 'nzvd', 'train', 'train')
     save_dataset(nzvd_val, 'nzvd', 'train', 'val')
     save_dataset(nzvd_test, 'nzvd', 'test', 'test')
     save_dataset(stan_train, 'stanford-cars', 'train', 'train')
     save_dataset(stan_val, 'stanford-cars', 'train', 'val')
 
-    classes_out = pd.DataFrame(np.hstack([all_bodies, list(range(len(all_bodies)))]))
-    colors_out = pd.DataFrame(np.hstack([all_colors, list(range(len(all_colors)))]))
+    classes_out = pd.DataFrame(np.stack([all_bodies, list(range(len(all_bodies)))], axis=1))
+    colors_out = pd.DataFrame(np.stack([all_colors, list(range(len(all_colors)))], axis=1))
     classes_out.to_csv(output_dir +'/classes.csv', index=False, header=False)
     colors_out.to_csv(output_dir + '/colors.csv', index=False, header=False)
--- data/src/utils.py
+++ data/src/utils.py
@@ -4,6 +4,7 @@ import os
 import shutil
 from common import input_dir, output_dir, TLHW, mappings
 
+
 def remove_extraneous(data):
     new_data = data[['file', 'top', 'left', 'height', 'width', 'body-type', 'categorical-color']]
     new_data.columns = ['file', 'top', 'left', 'height', 'width', 'body', 'color']
@@ -20,10 +21,35 @@ def tlhw_to_corners(data):
     return data
 
 def get_body_names(data):
-    return data[['body-type']].values
+    return data[['body']].values
 
 def get_colors_names(data):
-    return data[['categorical-color']].values
+    return data[['color']].values
+
+def align_stanford_classes(data, all_bodies):
+    data = data.values
+    class_id_idx = 5
+    rows = pd.read_csv(f'{input_dir}/stanford-cars/names.csv', header=None).values
+    bodies = np.array([row[0].split(' ')[-2].lower() for row in rows])
+    
+    # coerce some names
+    for old, new in mappings.items():
+        idxs = np.where(bodies == old)
+        bodies[idxs] = new
+    
+    # replace class id with class name
+    data[:,class_id_idx] = bodies[data[:,class_id_idx].astype(int) -1]
+    filtered_data = [data[i] for i in range(len(data)) if data[i,class_id_idx] in all_bodies]
+
+    
+    dummy_color =  np.repeat('black', (len(filtered_data)))
+    filtered_data = np.hstack([filtered_data, np.expand_dims(dummy_color,1)])
+    assert np.all([x in all_bodies for x in filtered_data[:,class_id_idx]]), "Bodies must all be in the specified set of bodies"
+
+    # set bounding boxes correctly
+    filtered_data = pd.DataFrame(filtered_data)
+    filtered_data.columns = ['file', 'x1', 'y1', 'x2', 'y2', 'body', 'color']
+    return filtered_data
 
 def split(data, prop=0.9):
     split_point = int(len(data) * prop)
@@ -44,34 +70,27 @@ def save_dataset(data, ds_name, folder, set_name):
     # copy images to outfolder
     copy_images(data, f'{input_dir}/{ds_name}/{folder}', img_dir)
     
-
     # append folder/ to fname
     data[['file']] = folder + '/' + data[['file']]
 
     # save csv
     data.to_csv(f'{out_dir}/{set_name}_annotations.csv', index=False, header=False)
 
+def merge_4x4s(data):
+    data = data.replace('4x4', 'suv')
+    return data
+
+def merge_greys(data):
+    data = data.replace('grey', 'silver')
+    return data
+
+def nzvd_pipeline(data):
+    data = remove_extraneous(data)
+    data = tlhw_to_corners(data)
+    data = merge_4x4s(data)
+    return data
+
 def mkdir(dir_):
     if not os.path.exists(dir_):
         os.mkdir(dir_)
-
-def align_stanford_classes(data, all_bodies):
-    data = data.values
-    class_id_idx = 5
-    rows = pd.read_csv(f'{input_dir}/stanford-cars/names.csv', header=None).values
-    bodies = np.array([row[0].split(' ')[-2].lower() for row in rows])
-    
-    # coerce some names
-    for old, new in mappings.items():
-        idxs = np.where(bodies == old)
-        bodies[idxs] = new
-    
-    # replace class id with class name
-    data[:,class_id_idx] = bodies[data[:,class_id_idx].astype(int) -1]
-    filtered_data = [data[i] for i in range(len(data)) if data[i,class_id_idx] in all_bodies]
-    
-    dummy_color =  np.repeat('black', (len(filtered_data)))
-    filtered_data = np.hstack([filtered_data, np.expand_dims(dummy_color,1)])
-    assert np.all([x in all_bodies for x in filtered_data[:,class_id_idx]]), "Bodies must all be in the specified set of bodies"
-    filtered_data = pd.DataFrame(filtered_data, columns=['file', *TLHW, 'body', 'color'])
-    return filtered_data
+        
