67b3d72b78aab59964abff30256b16de2e14c6ac
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-14 16:06:31 +1200

    add grey mreging to pipeline

--- data/src/utils.py
+++ data/src/utils.py
@@ -119,5 +119,6 @@ def nzvd_pipeline(data):
     data = remove_extraneous(data)
     data = tlhw_to_corners(data)
     data = merge_4x4s(data)
+    data = merge_greys(data)
     data = shuffle(data)
     return data
