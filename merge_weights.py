05e8b54317f38cbcef8ae8005a639e529f2773b0
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-26 10:26:23 +1200

    rename file, implement proper weights merging

--- merge_weights.py
+++ merge_weights.py
@@ -3,17 +3,25 @@ import argparse
 from model import efficientLPR
 import pandas as pd
 
+def freeze_model(model):
+    # freeze whole model
+    for layer in model.layers:
+        layer.trainable = False
+
 if __name__ == '__main__':
+    """
+    Script used to merge the trained portions of seperately trained
+    car detection and color classifier weights.
+    Also works around a Keras bug involving frozen weights loading weirdly.
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_path', help='path to full model trained weights', type=str, required=True)
-    parser.add_argument('--feature', help='whether to extract the color or body weights', choices=('color', 'body'), required=True)
+    parser.add_argument('--color_weights', help='full model weights with color trained', type=str, required=True)
+    parser.add_argument('--body_weights', help='full model weights with body trained', type=str, required=True)
     parser.add_argument('--phi', help="Phi model number", default=0, type=int, choices=(0, 1, 2, 3, 4, 5, 6))
     parser.add_argument('--class_path', help="Csv path to detection classes", type=str, required=True)
     parser.add_argument('--colors_path', help="Csv path to vehicle colors", type=str, required=True)
     parser.add_argument('--score_thresh', help="Score threshold for detections", default=0.2, type=float)
     args = parser.parse_args()
-
-    identifier = 'car_detector' if args.feature == 'body' else 'color_classifier'
 
     phi = args.phi
     score_threshold = args.score_thresh
@@ -29,13 +37,21 @@ if __name__ == '__main__':
                         num_colors=num_colors,
                         score_threshold=score_threshold)
 
-    # freeze whole model
-    for layer in model.layers:
-        layer.trainable = False
+    # unfreeze body and load its weights
+    freeze_model(model)
+    model.get_layer('car_detector').trainable = True
+    model.load_weights(args.body_weights)
+    body_weights = model.get_layer('car_detector').get_weights()
 
-    model.get_layer(identifier).trainable = True
+    # unfreeze color and load its weights
+    freeze_model(model)
+    model.get_layer('color_classifier').trainable = True
+    model.load_weights(args.color_weights)
+    color_weights = model.get_layer('color_classifier').get_weights()
 
-    model.load_weights(args.model_path)
+    # set color/body weights
+    model.get_layer('car_detector').set_weights(body_weights)
+    model.get_layer('color_classifier').set_weights(color_weights)
 
     # unfreeze model
     for layer in model.layers:
