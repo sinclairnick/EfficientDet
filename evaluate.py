6bfd7758f5e48e09dce01fcf0e80ba18f4a1aa1c
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-26 18:16:50 +1200

    add evaluation logic

--- evaluate.py
+++ evaluate.py
@@ -3,33 +3,59 @@ import pandas as pd
 import tensorflow as tf
 
 if __name__ == '__main__':
+    """Evaluate accuracy of color and body classification against ground truth"""
     parser = argparse.ArgumentParser('Evaluate the performance of the vehicle detection model')
-    parser.add_argument('--annotations_csv', help='Path to the ground truth annotations', required=True, type=str)
-    parser.add_argument('--car_csv', help='Path to csv file with color, body predictions', required=True, type=str)
-    parser.add_argument('--lp_csv', help='path to csv with lp character predictions', required=True, type=str)
+    parser.add_argument('--annotations_oath', help='Path to the ground truth annotations', required=True, type=str)
+    parser.add_argument('--predictions_path', help='Path to csv file with color, body predictions', required=True, type=str)
+    parser.add_argument('--classes_path', help='Path to csv file with classes', required=True, type=str)
+    parser.add_argument('--colors', help='Path to csv file with colors', required=True, type=str)
     args = parser.parse_args()
-
-    gt_data = pd.read_csv(args.annotations_csv, header=None).values
-    vehicle_data = pd.read_csv(args.car_csv, header=None).values
-    lp_data = pd.read_csv(args.lp_csv, header=None).values
-
-    # use .update_state() with these metrics
-    precision = tf.metrics.Precision()
-    recall = tf.metrics.Recall()
-    categorical_accuracy = tf.metrics.CategoricalAccuracy()
     
-    # 0 wrong, 1 wrong, ...
-    lp_accuracies = [tf.metrics.Accuracy() for x in range(7)]
+    classes = [x[0] for x in pd.read_csv(args.classes_path, header=None).values]
+    colors = [x[0] for x in pd.read_csv(args.colors_path, header=None).values]
 
-    # TODO: MERGE VEHICLE AND LP DATA
-    merged_data = []
+    class_labels = {x: i for i, x in enumerate(classes)}
+    color_labels = {x: i for i, x in enumerate(colors)}
+
+    gt_data = pd.read_csv(args.annotations_path, header=None)
+    gt_data.columns = ['file', 'x1', 'y1', 'x2', 'y2', 'body', 'color']
+    vehicle_data = pd.read_csv(args.predictions_path)
+
+    # assert headers are same order as classes/colors
+    color_headers = [x for x in vehicle_data.columns if x.startswith('color')]
+    class_headers = [x for x in vehicle_data.columns if x.startswith('body')]
+    assert all([x == color_headers[i].split('/')[1] for i,x in enumerate(colors)])
+    assert all([x == class_headers[i].split('/')[1] for i,x in enumerate(classes)])
+
+    class Metric:
+        def __init__(self):
+            self.precision = tf.metrics.Precision()
+            self.recall = tf.metrics.Recall()
+            self.categorical_accuracy = tf.metrics.CategoricalAccuracy()
+        def update_state(self, y_true, y_pred):
+            self.precision.update_state(y_true, y_pred)
+            self.recall.update_state(y_true, y_pred)
+            self.categorical_accuracy.update_state(y_true, y_pred)
+        def result(self):
+            return pd.DataFrame({
+                'precision': [self.precision.result().numpy()],
+                'recall': [self.recall.result().numpy()],
+                'categorical_accuracy': [self.categorical_accuracy.result().numpy()]
+            })
+    color_metric = Metric()
+    class_metric = Metric()
 
     # calculate performance metrics
-    for row in zip(merged_data, gt_data):
-        # TODO: compare the merged data to the ground truth
-        # COLOR/BODY: calculate precision, accuracy, whatever
-        # LP: calculate accuracy at different levels, e.g. 1 wrong, 2 wrong etc. to show how many are close but off
-        pass
+    for i in range(len(vehicle_data)):
+        predicted = vehicle_data.iloc[i]
+        ground = gt_data.iloc[i]
+        ground_class_label = class_labels[ground["body"]]
+        ground_color_label = color_labels[ground['color']]
+        assert predicted[['file']].values[0].split('/')[-1] == ground[['file']].values[0].split('/')[-1]
+        class_true = tf.one_hot(ground_class_label, depth=len(class_labels))
+        color_true = tf.one_hot(ground_color_label, depth=len(color_labels))
+        class_metric.update_state(class_true, predicted[class_headers])
+        color_metric.update_state(color_true, predicted[color_headers])
 
-    # import similarity metric
-    # evaluate matching/unmatching classifications to GT, wrt. similarty metric
+    print('Color Results:\n', color_metric.result())
+    print('Class Results:\n', class_metric.result())
