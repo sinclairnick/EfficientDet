fada043ee6e7ae2ae348c87afe330b1c325ccda3
Nick Sinclair <nicksinclair@xtra.co.nz>
2020-05-27 13:46:51 +1200

    fix typos, use tqdm

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,7 @@
 import argparse
 import pandas as pd
 import tensorflow as tf
+from tqdm import trange
 
 class Metric:
     def __init__(self):
@@ -21,10 +22,10 @@ class Metric:
 if __name__ == '__main__':
     """Evaluate accuracy of color and body classification against ground truth"""
     parser = argparse.ArgumentParser('Evaluate the performance of the vehicle detection model')
-    parser.add_argument('--annotations_oath', help='Path to the ground truth annotations', required=True, type=str)
+    parser.add_argument('--annotations_path', help='Path to the ground truth annotations', required=True, type=str)
     parser.add_argument('--predictions_path', help='Path to csv file with color, body predictions', required=True, type=str)
     parser.add_argument('--classes_path', help='Path to csv file with classes', required=True, type=str)
-    parser.add_argument('--colors', help='Path to csv file with colors', required=True, type=str)
+    parser.add_argument('--colors_path', help='Path to csv file with colors', required=True, type=str)
     args = parser.parse_args()
     
     classes = [x[0] for x in pd.read_csv(args.classes_path, header=None).values]
@@ -47,7 +48,7 @@ if __name__ == '__main__':
     class_metric = Metric()
 
     # calculate performance metrics
-    for i in range(len(vehicle_data)):
+    for i in trange(len(vehicle_data)):
         predicted = vehicle_data.iloc[i]
         ground = gt_data.iloc[i]
         ground_class_label = class_labels[ground["body"]]
